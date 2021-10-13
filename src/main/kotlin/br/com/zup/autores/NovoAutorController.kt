8fb5a4ea81935a5bb995495ba7669d6a4e263a78
tales-zup <tales.araujo@zup.com.br>
2021-10-13 15:06:46 -0300

    validacao sem anotacao no controller

--- src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
@@ -1,18 +1,28 @@
 package br.com.zup.autores
 
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
-import io.micronaut.validation.Validated
-import javax.validation.Valid
+import io.micronaut.validation.validator.Validator
+import jakarta.inject.Inject
+import javax.validation.ConstraintViolationException
 
-@Validated
 @Controller("/autores")
 class NovoAutorController {
 
+    @Inject
+    lateinit var validator: Validator
+
     @Post
-    fun cadastraAutor(@Body @Valid request: NovoAutorRequest) {
+    fun cadastraAutor(@Body request: NovoAutorRequest): NovoAutorRequest {
+        val violations = validator.validate(request)
+        if(violations.isNotEmpty())
+            throw ConstraintViolationException(violations)
+
         println(request)
+        return request
     }
 
 }
