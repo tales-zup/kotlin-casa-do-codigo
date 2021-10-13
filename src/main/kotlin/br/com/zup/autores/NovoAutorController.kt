2e427bedde0c329a848aff16966333f404224b59
tales-zup <tales.araujo@zup.com.br>
2021-10-13 13:59:01 -0300

    validacoes

--- src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
@@ -3,12 +3,15 @@ package br.com.zup.autores
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.validation.Validated
+import javax.validation.Valid
 
+@Validated
 @Controller("/autores")
 class NovoAutorController {
 
     @Post
-    fun cadastraAutor(@Body request: NovoAutorRequest) {
+    fun cadastraAutor(@Body @Valid request: NovoAutorRequest) {
         println(request)
     }
 
--- src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
@@ -1,7 +1,13 @@
 package br.com.zup.autores
 
+import io.micronaut.core.annotation.Introspected
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.Size
+
+@Introspected
 data class NovoAutorRequest (
-    val nome: String,
-    val email: String,
-    val descricao: String
+    @field:NotBlank val nome: String,
+    @field:NotBlank @field:Email val email: String,
+    @field:NotBlank @field:Size(max = 400) val descricao: String
 )
