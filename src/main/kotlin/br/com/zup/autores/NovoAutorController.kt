2ca3fb200ce00298ebcddf61ee0ca280b2addc6d
tales-zup <tales.araujo@zup.com.br>
2021-10-13 22:21:12 -0300

    persistencia

--- src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorController.kt
@@ -1,7 +1,5 @@
 package br.com.zup.autores
 
-import io.micronaut.http.HttpResponse
-import io.micronaut.http.MutableHttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
@@ -15,12 +13,16 @@ class NovoAutorController {
     @Inject
     lateinit var validator: Validator
 
+    @Inject
+    lateinit var autorRepository: AutorRepository
+
     @Post
     fun cadastraAutor(@Body request: NovoAutorRequest): NovoAutorRequest {
         val violations = validator.validate(request)
         if(violations.isNotEmpty())
             throw ConstraintViolationException(violations)
 
+        autorRepository.save(request.toModel())
         println(request)
         return request
     }
--- src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
+++ src/main/kotlin/br/com/zup/autores/NovoAutorRequest.kt
@@ -10,4 +10,8 @@ data class NovoAutorRequest (
     @field:NotBlank val nome: String,
     @field:NotBlank @field:Email val email: String,
     @field:NotBlank @field:Size(max = 400) val descricao: String
-)
+) {
+    fun toModel(): Autor {
+        return Autor(nome, email, descricao)
+    }
+}
